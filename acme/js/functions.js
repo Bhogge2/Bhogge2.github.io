50299e0d2550ef75e3497b42ccaca82159515c5a
Bhogge2 <hog15005@byui.edu>
2019-07-11 23:47:53 -0600

    almost done with project

--- acme/js/functions.js
+++ acme/js/functions.js
@@ -11,36 +11,81 @@ let navBar = document.getElementById("navBar");
 // Functions
 navBar.addEventListener('click', function (evt) {
 
-            // Get the city name
-            let pageName = evt.target.innerHTML;
-            switch (pageName) {
-                case "Anvils":
-                case "Explosives":
-                case "Decoys":
-                case "Traps":
-                    evt.preventDefault();
-                    break;
+    // Get the city name
+    let pageName = evt.target.innerHTML;
+    switch (pageName) {
+        case "Anvils":
+        case "Explosives":
+        case "Decoys":
+        case "Traps":
+            evt.preventDefault();
+            break;
+    }
+
+    let acmeURL = "/acme/js/acme.json";
+
+    fetch(acmeURL)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
             }
-
-            let acmeURL = "/acme/js/acme.json";
-
-            fetch(acmeURL)
-                .then(function (response) {
-                    if (response.ok) {
-                        return response.json();
-                    }
-                    throw new ERROR('Network response was not OK.');
-                })
-
-                .then(function (data) {
-                    // Check the data object that was retrieved
-                    console.log(data);
-                })
-
-                .catch(function (error) {
-                    console.log('There was a fetch problem: ', error.message);
-                    statusContainer.innerHTML = 'Sorry, the data could not be processed.';
-
-
-                })
+            throw new ERROR('Network response was not OK.');
         })
+
+        .then(function (data) {
+            // Check the data object that was retrieved
+            console.log(data);
+
+            // Create shortcut for object
+            let i = data[pageName];
+
+            // Create variable and input the object data into them
+
+            let description = i.description;
+            // Check
+            console.log(description);
+
+            let manufacturer = i.manufacturer;
+            // Check
+            console.log(manufacturer);
+
+            let reviews = i.reviews;
+            // Check
+            console.log(reviews);
+
+            let price = i.price;
+            // Check
+            console.log(price);
+
+            let name = i.name;
+            // Check
+            console.log(name);
+
+            let path = i.path;
+            // Check
+            console.log(path);
+
+            // Start inputting the info into the page
+            document.getElementById("itemName").innerHTML = name;
+            document.getElementById("itemDescription").innerHTML = description;
+            document.getElementById("manufacturer").innerHTML = manufacturer;
+            document.getElementById("price").innerHTML = price;
+            document.getElementById("reviewScore").innerHTML = reviews;
+            document.getElementById("itemImage").src = path;
+
+            // Change the page title
+            document.getElementById("pageTitle").innerHTML = "ACME | " + name;
+
+            // Display content pages and hide home page
+            contentPage.setAttribute('class', ''); 
+            homePage.setAttribute('class', 'hide'); 
+
+        })
+
+        .catch(function (error) {
+            console.log('There was a fetch problem: ', error.message);
+            statusContainer.innerHTML = 'Sorry, the data could not be processed.';
+
+
+        })
+})
